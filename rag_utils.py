53472052667ebb06763015ac9789f1c460f6e5c4
ky122643 <ky12090504@gmail.com>
2025-07-01 12:43:52 +0800

    Update rag_utils.py

--- rag_utils.py
+++ rag_utils.py
@@ -1,6 +1,6 @@
 import os
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 import streamlit as st
